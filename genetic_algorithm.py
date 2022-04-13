3aa46826872e5f121864b7eaeefe133914fd7e02
JadeQiong <volnut@163.com>
2022-04-13 18:00:54 +0800

    fix: some copy bug

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,18 +3,22 @@ from network import Network
 import genetic_algorithm_var
 import global_var
 import random
+import copy
 import numpy as np
 
-#遗传算法搜索对象：pe数量，pe阵列的行和列，拓扑结构
+# 遗传算法搜索对象：pe数量，pe阵列的行和列，拓扑结构
+
+
 class Population:
     def __init__(self, acc=Accelerator(), net=Network(), fit=0, r_fit=0, c_fit=0):
-        self.acc_gene = [acc.pe_num, acc.pe_size, acc.pe_numX, acc.pe_numY, acc.global_buf_size, acc.topo] #
+        self.acc_gene = [acc.pe_num, acc.pe_size, acc.pe_numX, acc.pe_numY, acc.global_buf_size, acc.topo]
         self.net_gene = [net.num_Layers, net.layers, net.layer_connection]
-        self.fit = fit #适应度
-        self.r_fit = r_fit#轮盘适应度
-        self.c_fit = c_fit
+        self.fit = fit  # 适应度
+        self.r_fit = r_fit   # 轮盘适应度
+        self.c_fit = c_fit   # 累计适应度
 
-#对pe数量编码
+# 对pe数量编码
+
 def encode_int(num):
     s = []
     while int(num) != 0:
@@ -23,20 +27,22 @@ def encode_int(num):
     dif = genetic_algorithm_var.int_code_len-len(s)
     for i in range(0, dif):
         s.append(0)
-    s.reverse()
-    #print(s)
+    # 把这个reverse先注释掉了
+    # s.reverse()
     return s
 
 
 def decode_int(s):
     num = 0
-    s.reverse()
+    # s.reverse()
     for i in range(0, len(s)):
         if s[i] == 1:
             num += pow(2, i)
-    #print(num)
+    # print(num)
     return num
-decode_int([0,0,0,1,0])
+
+# decode_int([0,0,0,1,0])
+
 
 def encode_float(num):
     return
@@ -105,15 +111,16 @@ def test_int_encoding():
             print(decode_int(encode_int(i)))
             print("test failed")
 
+
 class GeneticAlgorithm:
 
     def __init__(self, pop_num=10, iter_num=10, gen_num=10, pf=1):
-        self.pop_num = pop_num#种群数量
-        self.iter_num = iter_num#迭代次数
-        self.gen_num = gen_num#迭代一次的时候有多少代种群产生
+        self.pop_num = pop_num   # 种群数量
+        self.iter_num = iter_num  # 迭代次数
+        self.gen_num = gen_num   # 迭代一次的时候有多少代种群产生
         self.p_factor = pf      # punish factor
-        self.population = [Population() for i in range(self.pop_num)]#当前种群
-        self.next_population = [Population() for i in range(self.pop_num)]#下一代种群
+        self.population = [Population() for i in range(self.pop_num)]  #  当前种群
+        self.next_population = [Population() for i in range(self.pop_num)]   # 下一代种群
         self.best_pop = self.population[0]
 
     def run(self):
@@ -129,7 +136,8 @@ class GeneticAlgorithm:
             while gen < self.gen_num:
                 gen += 1
                 self.select()
-                for attr_id in range(0, len(genetic_algorithm_var.acc_gene_type)):
+                # pe_num 不用探索 = x*y, 处理完了记得pe_num = x*y
+                for attr_id in range(1, len(genetic_algorithm_var.acc_gene_type)):
                     attr = genetic_algorithm_var.acc_gene_type[attr_id]
                     self.crossover(attr_id, attr)
                 self.mutate()
@@ -144,32 +152,34 @@ class GeneticAlgorithm:
     def initiate(self):
         for p in self.population:
             # hardware init
-            for j in range(0, len(genetic_algorithm_var.acc_gene_type)):
+            for j in range(1, len(genetic_algorithm_var.acc_gene_type)):
                 gene = genetic_algorithm_var.acc_gene_type[j]
-                if gene == "pe_num":
-                    p.acc_gene[j] = random.randint(1, 10)
-                elif gene == "pe_size":
-                    p.acc_gene[j] = random.randint(1, 10)
+                # if gene == "pe_num":
+                #     p.acc_gene[j] = random.randint(1, )
+                if gene == "pe_size":
+                    p.acc_gene[j] = random.randint(1, 5)
                 elif gene == "pe_numX":
                     p.acc_gene[j] = random.randint(1, 5)
                 elif gene == "pe_numY":
                     p.acc_gene[j] = random.randint(1, 5)
                 elif gene == "global_buffer_size":
-                    p.acc_gene[j] = random.randint(1, 5)
+                    p.acc_gene[j] = random.randint(1, 10)
                 elif gene == "topo":
                     pass
-                    # print(p.acc_gene[j])
-                    # for i in p.acc_gene[j]:
-                    #     i = random.randint(0, 2)
+
+            p.acc_gene[0] = p.acc_gene[2] * p.acc_gene[3]
             # print(p.acc.topo)
             # software init
             # GG
-    #适者生存
+        return
+
+    # 保存当前最优解
     def keep_the_best(self):
         for p in self.population:
             if self.best_pop.fit < p.fit:
                 self.best_pop = p
 
+    # 适者生存
     def select(self):
         fit_sum = 0
         # 轮盘赌
@@ -183,70 +193,81 @@ class GeneticAlgorithm:
                 self.population[i].c_fit = self.population[i].fit
             else:
                 self.population[i].c_fit = self.population[i-1].c_fit + self.population[i].fit
+
         # generate random probability
         for i in range(0, len(self.population)):
-            probability = random.uniform(0, 1)
+            probability = random.random()
             if probability < self.population[0].c_fit:
-                self.next_population[i] = self.population[i]
+                self.next_population[i] = copy.deepcopy(self.population[i])
             else:
                 for j in range(0, len(self.population)-1):
                     if self.population[j].c_fit <= probability < self.population[j + 1].c_fit:
-                        self.next_population[i] = self.population[j+1]
+                        self.next_population[i] = copy.deepcopy(self.population[j+1])
+
         # update population
-        self.population = self.next_population
+        self.population = copy.deepcopy(self.next_population)
         return
-#满足突变条件则突变
+
+    # 选一对个体杂交
     def crossover(self, attr_id, attr):
         fir = sec = -1
         for i in range(0, len(self.population)):
-            if genetic_algorithm_var.crossover_rates[attr] > random.uniform(0, 1):
+            p = random.uniform(0, 1)
+            if genetic_algorithm_var.crossover_rates[attr] > p:
                 if fir != -1:
+                    # print("Y, "+str(p) + " < "+ str(genetic_algorithm_var.crossover_rates[attr]))
                     self.xover(attr_id, attr, fir, i)
                     fir = -1
                 else:
                     fir = i
         return
-    #双亲杂交产生新的个体
+
+    # 双亲杂交产生新的个体
     def xover(self, attr_id, attr,  i, j):
+        si = []
+        sj = []
         if attr != "topo":
             si = encode_int(self.population[i].acc_gene[attr_id])
             sj = encode_int(self.population[j].acc_gene[attr_id])
-            new_si = si
-            new_sj = sj
-            for k in range(0, 2):
-                tmp = new_si[k]
-                new_si[k] = new_sj[k]
-                new_sj[k] = tmp
-            # sometimes the value is set to 0, so max(1, num)
-            self.next_population[i].acc_gene[attr_id] = max(1, decode_int(new_si))
-            self.next_population[j].acc_gene[attr_id] = max(1, decode_int(new_sj))
         else:
-            pass
-        # if attr_type == "pe_num":
-        #     self.population[i].acc.pe_num, self.population[j].acc.pe_num = \
-        #         self.population[j].acc.pe_num, self.population[i].acc.pe_num
-        # elif attr_type == "pe_size":
-        #     self.population[i].acc.pe_size, self.population[j].acc.pe_size = \
-        #         self.population[j].acc.pe_size, self.population[i].acc.pe_size
-        #     pass
-        # elif attr_type == "pe_numX":
-        #     self.population[i].acc.pe_numX, self.population[j].acc.pe_numX = \
-        #         self.population[j].acc.pe_numX, self.population[i].acc.pe_numX
-        #     pass
-        # elif attr_type == "pe_numY":
-        #     self.population[i].acc.pe_numY, self.population[j].acc.pe_numY = \
-        #         self.population[j].acc.pe_numY, self.population[i].acc.pe_numY
-        #     pass
-        # elif attr_type == "global_buffer_size":
-        #     self.population[i].acc.global_buf_size, self.population[j].acc.global_buf_size = \
-        #         self.population[j].acc.global_buf_size, self.population[i].acc.global_buf_size
-        #     pass
-        # elif attr_type == "topo":
-            # self.population[i].acc.pe_num, self.population[j].acc.pe_num = \
-            #     self.population[j].acc.pe_num, self.population[i].acc.pe_num
-            # pass
+            si = encode_topo(self.population[i].acc_gene[attr_id])
+            sj = encode_topo(self.population[i].acc_gene[attr_id])
+
+        new_si = copy.deepcopy(si)
+        new_sj = copy.deepcopy(sj)
+
+        for k in range(0, 45 if attr == "topo" else 2):
+            tmp = new_si[k]
+            new_si[k] = new_sj[k]
+            new_sj[k] = tmp
+
+        if attr != "topo":
+            # 先试试交换
+            self.next_population[i].acc_gene[attr_id] = self.population[j].acc_gene[attr_id]
+            self.next_population[j].acc_gene[attr_id] = self.population[i].acc_gene[attr_id]
+            # self.next_population[i].acc_gene[attr_id] = max(1, decode_int(new_si))
+            # self.next_population[j].acc_gene[attr_id] = max(1, decode_int(new_sj))
+        else:
+            # acc_gene[0] is pe_num
+            nxt_numi = self.next_population[i].acc_gene[0]
+            nxt_numj = self.next_population[j].acc_gene[0]
+            self.next_population[i].acc_gene[attr_id] = decode_topo(new_si, nxt_numi)
+            self.next_population[j].acc_gene[attr_id] = decode_topo(new_sj, nxt_numj)
+
+        # print(" -杂交了------gen-i----- ")
+        # print(self.population[i].acc_gene[0:4])
+        # print(self.next_population[i].acc_gene[0:4])
+        # # print(" ------------- ")
+        # print(" -杂交了------gen--j---- ")
+        # print(self.population[j].acc_gene[0:4])
+        # print(self.next_population[j].acc_gene[0:4])
+        # print(" ------------- ")
+        self.next_population[i].acc_gene[0] = self.next_population[i].acc_gene[2] * self.next_population[i].acc_gene[3]
+        self.next_population[j].acc_gene[0] = self.next_population[j].acc_gene[2] * self.next_population[j].acc_gene[3]
+
         return
 
+    # 随机变异
     def mutate(self):
         for p in self.population:
             pass
@@ -336,27 +357,26 @@ class GeneticAlgorithm:
         self.initiate()
         self.evaluate()
         self.keep_the_best()
-        # self.crossover()
-
         for attr_id in range(0, len(genetic_algorithm_var.acc_gene_type)):
             attr = genetic_algorithm_var.acc_gene_type[attr_id]
             self.crossover(attr_id, attr)
+
         gen = 0
         for cur_iter in range(0, self.iter_num):
             while gen < self.gen_num:
                 gen += 1
                 self.select()
-                #对每个基因型进行交叉变异
+                # 对每个基因型进行交叉杂交
                 for attr_id in range(0, len(genetic_algorithm_var.acc_gene_type)):
                     attr = genetic_algorithm_var.acc_gene_type[attr_id]
                     self.crossover(attr_id, attr)
                 self.mutate()
                 self.evaluate()
-                # self.keep_the_best()
+                self.keep_the_best()
                 self.elitist()
-                print("best in each iteration----------------")
-                print(self.best_pop.acc_gene[0:4])
-                print(self.best_pop.net_gene[0:4])
+            print("----------------best in each iteration " + str(gen) + "----------------")
+            print(self.best_pop.acc_gene[0:4])
+            print(self.best_pop.net_gene[0:4])
         return
 
 
